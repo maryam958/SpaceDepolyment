e3a9440e5ee9b8fa47f503ff3b263c87f2a1557b
maryam958 <72671890+maryam958@users.noreply.github.com>
2023-05-06 21:29:32 +0300

    Deployment project

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ dotenv.config({ path: path.join(__dirname, "./config/.env") });
 
 import express from "express";
 import { globalError } from "./src/services/asyncHandler.js";
-import * as indexRouter from "./src/modules/index.route.js";
+import * as indexRouter from "./src/modules/index.router.js";
 import connection from "./Database/connection.js";
 import morgan from "morgan";
 
@@ -17,18 +17,19 @@ import morgan from "morgan";
 const app = express();
 
 const port =process.env.PORT 
+const baseUrl = process.env.BASEURL;
 
 
 app.use(express.json());
-
 app.use(cors({}))
+app.use(morgan("common"));
 
 
-if(process.env.ENV ==="DEV"){
-  app.use(morgan('dev'))
-}else{
-  app.use(morgan('common'))
-}
+// if(process.env.ENV ==="DEV"){
+//   app.use(morgan('dev'))
+// }else{
+//   app.use(morgan('common'))
+// }
 
 
 
@@ -38,12 +39,12 @@ app.get('/',(req,res)=>{
 })
 
 
-app.use(`${process.env.BASEURL}/user`, indexRouter.userRouter);
-app.use(`${process.env.BASEURL}/auth`, indexRouter.authRouter);
-app.use(`${process.env.BASEURL}/workingSpace`, indexRouter.workingSpaceRouter);
-app.use(`${process.env.BASEURL}/room`, indexRouter.roomRouter);
-app.use(`${process.env.BASEURL}/booking`, indexRouter.bookingRouter);
-app.use(`${process.env.BASEURL}/favorite`, indexRouter.favoriteRouter);
+app.use(`${baseUrl}/user`, indexRouter.userRouter);
+app.use(`${baseUrl}/auth`, indexRouter.authRouter);
+app.use(`${baseUrl}/workingSpace`, indexRouter.workingSpaceRouter);
+app.use(`${baseUrl}/room`, indexRouter.roomRouter);
+app.use(`${baseUrl}/booking`, indexRouter.bookingRouter);
+app.use(`${baseUrl}/favorite`, indexRouter.favoriteRouter);
 
 app.use("*", (req, res) => {
   res.send("In-valid Routing Plz check url  or  method");
