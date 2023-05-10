4f15a5810cc11255e0152429a5785960a8911315
maryam958 <72671890+maryam958@users.noreply.github.com>
2023-05-10 23:32:51 +0300

    Deployment project

--- src/modules/favorites/controller/favorites.controller.js
+++ src/modules/favorites/controller/favorites.controller.js
@@ -42,8 +42,9 @@ export const removeFavorites = asyncHandler(async (req, res, next) => {
 //make a function return favorites of the user
 export const getFavorites = asyncHandler(async (req, res, next) => {
   // let founded = await findById({ model: userModel, id: req.user._id,populate:('favorites') });
-  let founded = await findById({ model: userModel, id: req.user._id });
-  let ws=await findById({model:workSpaceModel,id:founded.favorites})
+  let founded = await findById({ model: userModel, id: req.user._id,populate:('favorites') });
+  // let ws=await findById({model:workSpaceModel,id:founded.favorites})
+
   if (!founded) {
     return next(new Error("user not found", { cause: 404 }));
   }
